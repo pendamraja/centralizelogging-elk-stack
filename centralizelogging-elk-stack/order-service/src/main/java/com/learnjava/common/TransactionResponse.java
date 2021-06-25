4ea160e416d86167ba036152ab827edc6dd25392
pendamraja <raja.pendam@gmail.com>
2021-06-25 12:28:16 +0530

    centralize logging

---
+++ centralizelogging-elk-stack/order-service/src/main/java/com/learnjava/common/TransactionResponse.java
@@ -0,0 +1,16 @@
+package com.learnjava.common;
+
+import com.learnjava.entity.Order;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionResponse {
+    private Order order;
+    private double amount;
+    private String transactionId;
+    private String message;
+}
---
+++ centralizelogging-elk-stack/payment-service/src/main/java/com/learnjava/repository/PaymentRepository.java
@@ -0,0 +1,8 @@
+package com.learnjava.repository;
+
+import com.learnjava.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment,Integer> {
+    Payment findByOrderId(int orderId);
+}
